e2828b1642975e687471335ee2e8f9deac8d87c5
Raian Medeiros <raiannolaco@gmail.com>
2024-04-30 18:35:58 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Servives/ServiceMessage.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Interfaces;
+using Domain.Interfaces.InterfacesServices;
+
+namespace Domain.Servives
+{
+    public class ServiceMessage : IServiceMessage
+    {
+        private readonly IMessage _IMessage;
+
+        public ServiceMessage(IMessage iMessage)
+        {
+            _IMessage = iMessage;
+        }
+
+
+    }
+}
---
+++ Infrastructure/Repository/Repositories/RepositoryMessage.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Interfaces;
+using Entities.Entities;
+using Infrastructure.Configuration;
+using Infrastructure.Repository.Generics;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repository.Repositories
+{
+    internal class RepositoryMessage : RepositoryGenerics<Message>, IMessage
+    {
+        private readonly DbContextOptions<ContextBase> _OptionBuilder;
+
+        public RepositoryMessage()
+        {
+            _OptionBuilder = new DbContextOptions<ContextBase>();
+        }
+    
+    }
+}
